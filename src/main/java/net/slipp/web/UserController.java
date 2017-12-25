949f6d66f4a7ef7b04f68607f5e438b07f48b068
ponderingvay <ponderingvay@gmail.com>
2017-12-25 03:29:32 -0500

    H2, JPA dependencies added User.java moved to domain package

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import net.slipp.domain.User;
+
 @Controller
 public class UserController {
 	
