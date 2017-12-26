e411b29ae6a5be1220c4a4bf1ce641bcb520a6ff
ponderingvay <ponderingvay@gmail.com>
2017-12-26 02:21:32 -0500

    Mustache include (partials) added: include>navigation {{>include/navigation}}

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -8,27 +8,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.slipp.domain.User;
 import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	private List<User> users = new ArrayList<User>();
 	
 	@Autowired
 	private UserRepository userRepository;
 	
-	@PostMapping("/user/create")
+	@PostMapping("")
 	public String create(User user) {
 	System.out.println();
 	System.out.println("user : " + user);
 	users.add(user);
 	userRepository.save(user);
-	return "redirect:/user/list";
+	return "redirect:/users";
 	}
 
-	@GetMapping("/user/list")
+	@GetMapping("")
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
 		return "user/list";
